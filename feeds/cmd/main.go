1f572a945f67f1a8fac36c568c9ff9105fd690c3
Jordan Reger <mail@jordanreger.com>
2024-05-08 11:30:25 -0400

    feeds seem to work

--- feeds/cmd/main.go
+++ feeds/cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"jordanreger.com/bsky/feeds"
 	"jordanreger.com/bsky/feeds/algorithms"
@@ -32,9 +33,22 @@ func main() {
 
 	mux.HandleFunc("/xrpc/app.bsky.feed.getFeedSkeleton", func(w http.ResponseWriter, r *http.Request) {
 		util.ContentType(w, "application/json")
-		//uri := r.URL.Query().Get("feed")
 
-		feed := algorithms.Static()
+		uri := r.URL.Query().Get("feed")
+		uri_split := strings.Split(uri, "/")
+		feed_name := strings.Replace(uri_split[len(uri_split)-1], "wx", "", 1)
+		record_type := uri_split[len(uri_split)-2]
+		request_did := uri_split[len(uri_split)-3]
+
+		var feed []feeds.Post
+
+		feed, err := algorithms.State(feed_name)
+
+		// no feed or incorrect request
+		if err != nil || record_type != "app.bsky.feed.generator" || request_did != did {
+			fmt.Fprint(w, `{"error": "feed not found"}`)
+			return
+		}
 
 		fmt.Fprint(w, feeds.GetFeedSkeleton(feed))
 		return
